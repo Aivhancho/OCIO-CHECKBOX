956b601773f935cc0417b99dad3aed9184bcf701
ivan <falso@falso.es>
2024-03-01 13:32:22 +0100

    he añadido checkbox

--- src/main/java/com/example/ocio_checkbox/OcioController.java
+++ src/main/java/com/example/ocio_checkbox/OcioController.java
@@ -1,14 +1,33 @@
 package com.example.ocio_checkbox;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
 
 public class OcioController {
+
+    @FXML
+    private CheckBox cine;
+
+    @FXML
+    private CheckBox musica;
+
+    @FXML
+    private CheckBox videojuegos;
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void onEnviar(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Información");
+        alert.setHeaderText(null);
+        alert.setContentText("Mensaje");
+        alert.showAndWait();
     }
+
 }
+
